c152e86faa4fa0833510a869b0a6778309d16ecf
Franck Dernoncourt <franck.dernoncourt@gmail.com>
2015-07-19 12:06:55 -0700

    test

--- test.py
+++ test.py
@@ -3,11 +3,10 @@ def main():
     This is the main function Really?
     '''
     i = 0 + 1
-    print(i+2)
+    print(i+5)
     j = 2
     print(j)
 
 if __name__ == "__main__":
     main()
     #cProfile.run('main()') # if you want to do some profiling
-cd 
