08e3df1e6784cdeb8c700bc9793ba10560e7281a
Franck Dernoncourt <franck.dernoncourt@gmail.com>
2015-07-19 11:46:10 -0700

    add py

---
+++ test.py
@@ -0,0 +1,10 @@
+def main():
+    '''
+    This is the main function
+    '''
+    i = 0
+    print(i)
+
+if __name__ == "__main__":
+    main()
+    #cProfile.run('main()') # if you want to do some profiling
